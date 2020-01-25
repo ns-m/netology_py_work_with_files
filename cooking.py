1c47dfc58b32a03d2f7ff86ee7741dd5411bb374
KMV <9117479@gmail.com>
2020-01-25 23:15:28 +0300

    Cook

--- cooking.py
+++ cooking.py
@@ -1,1 +1,3 @@
 from read_file_class import ReadRecipesFile
+cook = ReadRecipesFile()
+cook
--- read_file_class.py
+++ read_file_class.py
@@ -1,2 +1,12 @@
-class ReadRecipesFile:
-    pass
+
+def read_file_rec():
+    cook_dict = {}
+    with open('recipes.txt', encoding='utf-8') as txt:
+        while True:
+            dishes_name = txt.readline()[1]
+        for line in txt:
+            key, *value = line
+            cook_dict[key] = value
+        print(cook_dict)
+
+read_file_rec()
