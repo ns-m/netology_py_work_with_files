c378c9d8c9de27a0527b290ddd1fad7deeb2582f
kmv <9629095@gmail.com>
2020-01-27 17:06:36 +0300

    File read and (dishes, person_count) add

--- cooking.py
+++ cooking.py
@@ -1,1 +1,19 @@
 from read_file_class import ReadRecipesFile
+cooking = ReadRecipesFile()
+
+# cook_dict = {cooking.__dict__}
+# for cook in cook_dict:
+#     print(cook.keys())
+
+def get_shop_list_by_dishes(dishes, person_count):
+    count_dishes = int(input('Input dishes count : '))
+    count = 0
+    ing_dishes = []
+    while count < count_dishes:
+        dishes = input('Input dishes : ')
+        count += 1
+        ing_dishes.append(cooking.__dict__[dishes])
+    person_count = int(input('Input person count : '))
+    print(ing_dishes, person_count)
+
+get_shop_list_by_dishes(0, 2)
--- read_file_class.py
+++ read_file_class.py
@@ -1,2 +1,28 @@
+from itertools import islice
+
 class ReadRecipesFile:
-    pass
+    def __init__(self):
+        with open('recipes.txt', encoding='utf-8') as f:
+
+            while True:
+                cook_book = {}
+                n = f.readline().rstrip()
+                try:
+                    c = int(f.readline().rstrip())
+                except ValueError:
+                    pass
+                d_n = list(islice(f, c))
+                f.readline()
+
+                if not n or not d_n:
+                    break
+
+                ingrs = []
+                for d in d_n:
+                    value = d.split(' | ')
+                    val = {'ingredient_name': value[0], 'quantity': int(value[1]),
+                           'measure': value[2].replace('\n', '')}
+                    ingrs.append(val)
+                cook_book[n] = [*ingrs]
+
+                print(cook_book)
