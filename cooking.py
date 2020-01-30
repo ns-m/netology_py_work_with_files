f23136b106c47eb440b9ac126f33ef6f8f5c154f
KMV <9117479@gmail.com>
2020-01-30 22:41:53 +0300

    Release

--- cooking.py
+++ cooking.py
@@ -1,33 +1,35 @@
 from read_file_class import ReadRecipesFile
 import sys
+from pprint import pprint
 
 cooking = ReadRecipesFile()
 
+
 def get_shop_list_by_dishes():
     count_dishes = int(input('Input the quantity of dishes : '))
-    count = 0
-    ing_dishes = []
-    menu = {}
+    count, count2, count3 = 0, 0, 0
+    ing_dishes, menu2, menu3 = [], [], []
+    menu_dict = {}
     while count < count_dishes:
         dishes = input(f'Input the name of dishes №{count+1} : ')
         count += 1
         try:
-            # ing_dishes.append(cooking.cook_book[dishes])
-            menu.update(cooking.cook_book[dishes])
+            ing_dishes.append(cooking.cook_book[dishes])
         except:
             print('No such dish!')
             sys.exit()
     person_count = int(input('Enter the number of people : '))
-    count2 = 0
-    # for ing in ing_dishes:
-    #     print(ing)
-    for x in ing_dishes[0]:
-        for k, v in x.items():
-            print(k, v)
-        # k = ing[count2]['ingredient_name']
-        # v = ing[count2].items()
+    while count2 < len(ing_dishes):
+        for x in ing_dishes[count2]:
+            for k, v in x.items():
+                menu2.append(k)
+                menu3.append(v)
+        count2 += 1
+    while count3 <= len(menu3)-1:
+        menu4 = {menu3[count3]:{'measure': menu3[count3+2], 'quantity':menu3[(count3+1)]*person_count}}
+        count3 += 3
+        menu_dict.update(menu4.copy())
+    pprint(menu_dict)
 
-
-    print(menu)
 
 get_shop_list_by_dishes()
--- read_file_class.py
+++ read_file_class.py
@@ -1,27 +1,29 @@
 from itertools import islice
 
+
 class ReadRecipesFile:
+
     def __init__(self):
         with open('recipes.txt', encoding='utf-8') as f:
             cook_book = {}
             while True:
-                n = f.readline().rstrip()
+                dish_name = f.readline().rstrip()
                 try:
-                    c = int(f.readline().rstrip())
+                    amount_of_ingrs = int(f.readline().rstrip())
                 except ValueError:
                     pass
-                d_n = list(islice(f, c))
+                name_of_ingrs = list(islice(f, amount_of_ingrs))
                 f.readline()
 
-                if not n or not d_n:
+                if not dish_name or not name_of_ingrs:
                     break
 
                 ingrs = []
-                for d in d_n:
-                    value = d.split(' | ')
+                for name in name_of_ingrs:
+                    value = name.split(' | ')
                     val = {'ingredient_name': value[0], 'quantity': int(value[1]),
                            'measure': value[2].replace('\n', '')}
                     ingrs.append(val)
-                    cook_book[n] = [*ingrs]
+                    cook_book[dish_name] = [*ingrs]
 
                 self.cook_book = cook_book
